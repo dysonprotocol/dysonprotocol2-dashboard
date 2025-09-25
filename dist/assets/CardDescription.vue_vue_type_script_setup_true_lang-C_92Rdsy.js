fa08f1dbea2627ad86554d052909da919dbe183b
Your Name <you@example.com>
2025-09-25 04:23:29 +0000

    build

---
+++ dist/assets/CardDescription.vue_vue_type_script_setup_true_lang-C_92Rdsy.js
@@ -0,0 +1,1 @@
+import{d as t,c as o,n as r,x as n,ae as c,a8 as l,o as p}from"./index-DC5p1QS8.js";const i=t({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,d)=>(p(),o("p",{"data-slot":"card-description",class:r(n(c)("text-muted-foreground text-sm",e.class))},[l(a.$slots,"default")],2))}});export{i as _};
